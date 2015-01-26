8f679e9bef2f9448f56e198ded1ad004b9823d28
Wael Showair <show0017@algonquinlive.com>
2015-01-25 22:48:36 -0500

    initial website

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Basic Form</title>
+    </head>
+    <body>
+        <form action="form_processing.php" method="post">
+            Username: <input type="text" name="username" value=""><br>
+            Password: <input type="password" name="password" value=""><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </body>
+</html>
